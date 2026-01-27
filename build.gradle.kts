772b9e8afccc8155fe877a38e26b40caa04f7bb3
Florian Cramer <florian.cramer.112@gmail.com>
2026-01-27 22:37:09 +0100

    Bump kotlin from 2.2.21 to 2.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,10 @@ import org.springframework.boot.gradle.plugin.SpringBootPlugin
 
 plugins {
     id("org.springframework.boot") version "4.0.1"
-    kotlin("jvm") version "2.2.21"
-    kotlin("kapt") version "2.2.21"
-    kotlin("plugin.spring") version "2.2.21"
-    kotlin("plugin.jpa") version "2.2.21"
+    kotlin("jvm") version "2.3.0"
+    kotlin("kapt") version "2.3.0"
+    kotlin("plugin.spring") version "2.3.0"
+    kotlin("plugin.jpa") version "2.3.0"
 
     id("org.jlleitschuh.gradle.ktlint") version "14.0.1"
     id("dev.detekt") version "2.0.0-alpha.1"
@@ -28,6 +28,9 @@ repositories {
 }
 
 dependencies {
+    implementation(enforcedPlatform("org.jetbrains.kotlin:kotlin-bom:${kotlin.coreLibrariesVersion}"))
+    kapt(enforcedPlatform("org.jetbrains.kotlin:kotlin-bom:${kotlin.coreLibrariesVersion}"))
+    developmentOnly(enforcedPlatform("org.jetbrains.kotlin:kotlin-bom:${kotlin.coreLibrariesVersion}"))
     implementation(enforcedPlatform(SpringBootPlugin.BOM_COORDINATES))
     kapt(enforcedPlatform(SpringBootPlugin.BOM_COORDINATES))
     developmentOnly(enforcedPlatform(SpringBootPlugin.BOM_COORDINATES))
